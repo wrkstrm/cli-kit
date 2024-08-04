ae6776596ea564672e6bd9410e922d9446716f28
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2024-08-04 00:53:56 -0700

    Add gitignore.

--- Package.swift
+++ Package.swift
@@ -1,6 +1,4 @@
-// swift-tools-version:5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.10
 import PackageDescription
 
 let package = Package(
@@ -16,9 +14,6 @@ let package = Package(
     .package(url: "https://github.com/apple/swift-log.git", from: "1.5.3"),
   ],
   targets: [
-    // Targets are the basic building blocks of a package. A target can define a module or a test
-    // suite. Targets can depend on other targets in this package, and on products in packages which
-    // this package depends on.
     .executableTarget(
       name: "Toolbox",
       dependencies: [
