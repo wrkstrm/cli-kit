7fe7e82c9a9ad59da41d64e9e398d454723bc535
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2024-08-05 11:23:55 -0700

    Standardize argument parser imports.

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     .executable(name: "tb", targets: ["Toolbox"])
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.3"),
+    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0"),
     .package(url: "https://github.com/apple/swift-log.git", from: "1.5.3"),
   ],
   targets: [
